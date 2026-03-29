9afd312bb95444aab1fc2e1f4e96e1f7af028afb
Gokul <gokulnath1745@gmail.com>
2026-03-29 16:00:23 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,11 +4,6 @@ cd /home/ec2-user/django-app
 
 source venv/bin/activate
 
+echo "Starting Django server..."
 
-python manage.py migrate
-
-
-python manage.py collectstatic --noinput
-
-
-nohup python manage.py runserver 0.0.0.0:8000 &
+nohup python manage.py runserver 0.0.0.0:8000 > output.log 2>&1 &
