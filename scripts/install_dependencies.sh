4d442083d5cc59c99a34a85e48a67387a3a2f989
Gokul <gokulnath1745@gmail.com>
2026-03-29 15:59:59 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+echo "Updating system..."
+sudo yum update -y
+
+echo "Installing Python3..."
+sudo yum install python3 -y
+
+echo "Installing pip..."
+sudo yum install python3-pip -y
+
+echo "Installing virtualenv..."
+pip3 install virtualenv
+
 cd /home/ec2-user/django-app
 
-
+echo "Creating virtual environment..."
 python3 -m venv venv
-
 
 source venv/bin/activate
 
-
+echo "Installing requirements..."
 pip install -r requirements.txt
